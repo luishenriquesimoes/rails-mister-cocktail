49f9560ff07d79db53ca1b6dcb8c236ab3a9d1cb
luis <luishenriquesimoes@yahoo.com.br>
2020-11-30 15:27:48 -0300

    routes and controllers created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts 'Destroying previous records...'
+
+Ingredient.destroy_all
+
+puts 'DB is clean'
+puts 'creating ingredients'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+data_serialized = open(url).read
+data = JSON.parse(data_serialized)
+data['drinks'].each do |drink|
+  ingredient = Ingredient.create(name: drink['strIngredient1'])
+  puts "Created ingredient number #{ingredient.id} with name #{ingredient.name}."
+end
